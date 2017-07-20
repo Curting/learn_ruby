b549af030eebc5f2b5937cd108e28653fb8976df
Oliver <oc@hejverden.dk>
2017-07-20 16:25:51 -0400

    Add solutions for 00 and 01

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,13 @@
 #write your code here
+
+# Fahrenheit to Celsius.
+# 1 degree fahrenheit = 5/9 degree celsius
+# Use floats
+
+def ftoc(fahrenheit)
+	(fahrenheit - 32) * 5/9.to_f
+end
+
+def ctof(celsius)
+	celsius * 9/5.to_f + 32
+end
