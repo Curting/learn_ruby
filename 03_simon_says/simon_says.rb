2e4ddbb889b68812964f9892120bc115c6641c73
Oliver <oc@hejverden.dk>
2017-07-22 14:30:17 -0400

    Add solution for simon_says.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,48 @@
 #write your code here
+
+def echo(string)
+	string
+end
+
+def shout(string)
+	string.upcase
+end
+
+def repeat(string, n = 2)
+	([string] * n).join(" ")
+end
+
+def start_of_word(string, n)
+	string.slice(0, n)
+end
+
+def first_word(string)
+	string.split.first
+end
+
+def titleize(string)
+	# String to array with all words downcased
+	string_array = string.downcase.split(" ")
+
+	# Iterate through array and capitalize all words expect "little words"
+	string_array.each do |word|
+		case word
+		when "a", "an", "the", "and", "but", "or", "for", "nor", "on", "at", "to", "from", "by",
+			"over", "under"
+			
+			# If it's a small word, skip it and go to the next one
+			next
+		else
+			word.capitalize!
+		end
+	end
+
+	# Capitalize the first and last word
+	string_array.first.capitalize!
+	string_array.last.capitalize!
+
+	# Put the array back together to form a string
+	string = string_array.join(" ")
+end
+
+
