790602f605f6ab03caf49453bf3eea310a77623e
Oliver <oc@hejverden.dk>
2017-07-25 15:06:12 -0400

    Add solution to 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
-# write your code here
+	def title=(title)
+		@title = titleize(title)
+	end
+
+	def title
+		@title
+	end
+
+	def titleize(str)
+		small_words = %w[the a an and in of]
+
+		str.capitalize.gsub(/\w+/) do |word|
+			if small_words.include?(word)
+				word
+			else
+				word.capitalize
+			end
+		end
+	end
 end
+
+
